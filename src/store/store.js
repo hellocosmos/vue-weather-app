a635ac5d7eb95efe7f10c96ac14e6b55c5fc0903
hellococosmos <hellocosmos@gmail.com>
2025-01-09 18:58:09 +0900

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -10,7 +10,7 @@ export default createStore({
         location: "location",
         city: "Seoul",
     },
-    toggle: false,
+    toggle: true,
   },
   mutations: {
     updateWeather(state, payload) {
